b1f9bf0f02824444766baacf6fec3feb1940ff29
Dedi-edot <id.dedi81@gmail.com>
2021-07-31 07:41:20 +0700

    Set Notifications ForeGround and BackGround

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const notifyMeBtnHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "The First Notification",
+        body: "Assalamualaikum",
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
+
+  // Get Permissions
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS)
+      .then((permObj) => {
+        if (permObj.status !== "granted") {
+          return Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+        return permObj;
+      })
+      .then((permObj) => {
+        if (permObj.status !== "granted") {
+          return permObj;
+        }
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }, []);
+
+  // Set Notifications Listener
+  useEffect(() => {
+    const subNotifForeground = Notifications.addNotificationReceivedListener(
+      (notifData) => {
+        return console.log("Foreground", notifData);
+      }
+    );
+
+    const subNotifBackground =
+      Notifications.addNotificationResponseReceivedListener((res) => {
+        return console.log("Background", res);
+      });
+
+    return () => {
+      subNotifForeground.remove();
+      subNotifBackground.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={notifyMeBtnHandler}>
+        <Text>Notify Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +75,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
